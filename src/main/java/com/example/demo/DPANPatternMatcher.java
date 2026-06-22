0f388ad4d422c85ef2821511b7f8a918ff026124
rezcova1808-alt <rezcova1808@gmail.com>
2026-06-23 00:05:03 +0300

    fix package name

--- src/main/java/com/example/demo/DPANPatternMatcher.java
+++ src/main/java/com/example/demo/DPANPatternMatcher.java
@@ -1,4 +1,4 @@
-package com.example.excelparesernew;
+package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/example/demo/StopParsingException.java
+++ src/main/java/com/example/demo/StopParsingException.java
@@ -1,4 +1,4 @@
-package com.example.excelparesernew;
+package com.example.demo;
 
 final class StopParsingException extends RuntimeException {
 
